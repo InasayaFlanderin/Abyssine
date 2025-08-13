468cc95059c9e30cc374be9b188bc32dd376c900
InasayaFlanderin <InasayaFlanderin@gmail.com>
2025-08-13 22:34:20 +0700

    Starting PRNG

---
+++ src/main/java/personal/inasayaflanderin/abyssine/common/random/RandomNumberGenerator.java
@@ -0,0 +1,5 @@
+package personal.inasayaflanderin.abyssine.common.random;
+
+public interface RandomNumberGenerators {
+
+}
---
+++ src/main/java/personal/inasayaflanderin/abyssine/common/random/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package is about random number generator and managining the randomness
+ * */
+
+package personal.inasayaflanderin.abyssine.common.random;
